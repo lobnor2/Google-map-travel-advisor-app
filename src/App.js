2f9bcaf4c9df6593dc5a0e4f2dd9ed15ccdbaeb8
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-25 00:20:42 +0530

    added main components and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+import { CssBaseline, Grid } from "@material-ui/core";
 
 function App() {
-  return <div className="App">hellow world</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      {/* take full 12 on small devices and 4 on medium and larger devices */}
+      <Grid
+        container
+        spacing={3}
+        style={{ width: "100%", border: "1px solid red" }}
+      >
+        <Grid item xs={12} md={4} style={{ border: "1px solid green" }}>
+          <List />
+        </Grid>
+        {/* take full 12 on small devices and 8 spaces for medium and larger devices */}
+        <Grid item xs={12} md={8} style={{ border: "1px solid green" }}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
