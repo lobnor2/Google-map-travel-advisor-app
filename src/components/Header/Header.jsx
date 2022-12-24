fdc11089637e96169243d5d2f090bf0f9febf81d
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-25 01:29:43 +0530

    added half header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Box, Typography, InputBase } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+      </Toolbar>
+      <Box display="flex">
+        <Typography variant="h6" className={classes.title}>
+          Explore New Places
+        </Typography>
+        {/* <Autocomplete> */}
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search..."
+            classes={{ root: classes.inputRoot, input: classes.inputInput }}
+          />
+        </div>
+        {/* </Autocomplete> */}
+      </Box>
+    </AppBar>
+  );
 };
 
 export default Header;
